6a9f9c89edda9c190c138881ffeb0a5a64966126
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-04-29 22:17:35 -0400

    loading component changes

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,40 @@
+/**
+ * 
+ * @param type - the type of loading animation wanted
+ * @returns a loading animation
+ */
 export const Loading = (
     {
         type
     }:{
-        type: string
+        type: string | undefined
     }) => {
-    return(
-        type === "nextQuestion"
-        ? <h1>Determining Next Question</h1>
-        : type === "generatingQuestions"
-        ? <h1>Generating Next Question Set</h1>
-        : type === "finalReport"
-        ? <h1>Generating Final Career Report</h1>
-        : <h1>{`Loading ${type}`}</h1>
-    )
+
+    switch(type) {
+        case "nextQuestion":
+            return <NextQuestionLoading/>
+        case "generatingQuestion":
+            return <GeneratingQuestionLoading/>
+        case "finalReport":
+            return <FinalReportLoading/>
+        default:
+            return <GeneralLoading/>
+    }
+}
+
+const NextQuestionLoading = () => {
+    return <h1>Determining Next Question</h1>
+}
+
+const GeneratingQuestionLoading = () => {
+    return <h1>Generating Next Question Set</h1>
+}
+
+const FinalReportLoading = () => {
+    return <h1>Generating Final Career Report</h1>
+}
+
+const GeneralLoading = () => {
+    // the loading text here is temperary
+    return <div className="general-loading-animation">Loading</div>
 }
