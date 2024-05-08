ab5fc771310bf268443baad0160a36bf256849a7
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-05-08 15:44:51 -0400

    styling colors on progress bar. label to bar added

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,7 @@
 export const ProgressBar = ({value, max} : {value : number, max : number}):JSX.Element => {
     return(
         <div>
+            <p className="quiz-progress-bar-text">{`${value} / ${max}`}</p>
             <progress className="quiz-progress-bar" value={value} max={max}></progress>
         </div>
     )
